61289ecceede346d5c5aedda355fc485a2023ca9
Alvaro Molina Jara <22986107+AlvaroMolinaCL@users.noreply.github.com>
2026-06-21 16:55:24 -0400

    Se actualizan rutas de api.php

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,9 @@ use App\Http\Controllers\Api\ProductoController;
 use App\Http\Controllers\Api\FavoritoController;
 use App\Http\Controllers\Api\ReporteController;
 use App\Http\Controllers\Api\ConsejoController;
+use App\Http\Controllers\Api\CategoriaComidaController;
+use App\Http\Controllers\Api\InformacionNutricionalController;
+use App\Http\Controllers\Api\CompraController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -16,6 +19,12 @@ Route::get('/user', function (Request $request) {
 Route::apiResource('locales', LocalController::class);
 Route::apiResource('menus', MenuController::class);
 Route::apiResource('productos', ProductoController::class);
+Route::apiResource('categorias-comida', CategoriaComidaController::class);
+Route::apiResource('informacion-nutricional', InformacionNutricionalController::class);
+Route::post('compras/registrar', [CompraController::class, 'registrar'])->middleware('auth:sanctum');
+Route::apiResource('compras', CompraController::class);
+Route::get('productos/{id}/informacion-nutricional', [ProductoController::class, 'informacionNutricional']);
+Route::put('productos/{id}/categorias', [ProductoController::class, 'syncCategorias']);
 Route::apiResource('favoritos', FavoritoController::class);
 Route::apiResource('reportes', ReporteController::class);
 Route::apiResource('consejos', ConsejoController::class);
